250b4db54fcc6a315c73ec702be1ce04e3f0870b
ERACORP-ID <128222592+ERACORP-ID@users.noreply.github.com>
2023-03-19 17:27:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -65,9 +65,9 @@ global.myytv = 'https://youtu.be/FAsL-Jy4qLc'
 global.mygc = "https://chat.whatsapp.com/Cp1OJenk6Q9D9vgLjLU558"
 
 // Engak Ngaruh, Belum Kepasang
-global.botname = 'NAZE BOT'
-global.akulaku = 'Bot By Naze'
-global.ytname = 'YT NAZE' //Kalo Ngk Punya Yt Ngak Usah Di Ganti
+global.botname = 'EraBotz'
+global.akulaku = 'Bot By Zero'
+global.ytname = 'YT ZERODEV' //Kalo Ngk Punya Yt Ngak Usah Di Ganti
 
 
 let file = require.resolve(__filename)
